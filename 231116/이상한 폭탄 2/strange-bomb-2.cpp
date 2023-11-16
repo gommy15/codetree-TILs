f10bdaa017aa0fdc0fca6969214a3999c7bd8b39
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2023-11-16 19:52:10 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231116/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n, k;
+int arr[101];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>k;
+
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int max_num = -1;
+
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<=i+k; j++) {
+            if(arr[i] == arr[j]) {
+                max_num = arr[i];
+            }
+        }
+    }
+
+    cout << max_num;
+    return 0;
+}
