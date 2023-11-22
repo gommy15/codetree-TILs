4a708913504964f817152e114ef947cecdc5bdff
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2023-11-22 17:43:53 +0900

    Title: 스승의 은혜 3, Time: 31ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_STU 1000
+
+int n, b;
+pair<int, int> student[MAX_STU];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>b;
+    int price, deliv;
+    for(int i=0; i<n; i++) {
+        cin >> price>>deliv;
+        student[i] = make_pair(price, deliv);
+    }
+
+    int max_stunum = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        pair<int, int> tmp[MAX_STU] = {};
+        for(int j = 0; j < n; j++) {
+            price = student[j].first;
+            deliv = student[j].second;
+            if(i == j) price = price/2;
+
+            tmp[j] = make_pair(price, deliv);
+        }
+
+        sort(tmp, tmp + n);
+
+        int stu_num = 0;
+        int cnt = 0;
+
+        for(int j = 0; j < n; j++) {
+            if(cnt + tmp[j].first + tmp[j].second > b)
+                continue;
+            cnt += (tmp[j].first + tmp[j].second);
+            stu_num++;
+        }
+
+        max_stunum = max(max_stunum, stu_num);
+    }
+
+    cout << max_stunum;
+    return 0;
+}
