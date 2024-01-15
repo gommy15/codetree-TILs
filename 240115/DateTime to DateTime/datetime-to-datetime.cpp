0e4a41576d0aabb50093355f6c2a8befab302fd1
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-15 12:12:48 +0900

    Title: DateTime to DateTime, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240115/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240115/DateTime to DateTime/datetime-to-datetime.cpp
@@ -7,23 +7,29 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> a>>b>>c;
 
-    int day = 11, hour=11, min=11, time = 0;
-    while(true) {
-        if(day == a && hour == b && min == c) break;
+    int diff = (a*24*60 + b*60+c) - (11*24*60 + 11*60 + 11);
 
-        time++;
-        min++;
+    cout << diff;
 
-        if(min == 60) {
-            hour++;
-            min = 0;
-            if(hour == 24) {
-                day++;
-                hour=0;
-            }
-        }
-    }
+    // 시초
+    // int day = 11, hour=11, min=11, time = 0;
 
-    cout << time;
+    // while(true) {
+    //     if(day == a && hour == b && min == c) break;
+
+    //     time++;
+    //     min++;
+
+    //     if(min == 60) {
+    //         hour++;
+    //         min = 0;
+    //         if(hour == 24) {
+    //             day++;
+    //             hour=0;
+    //         }
+    //     }
+    // }
+
+    // cout << time;
     return 0;
 }
