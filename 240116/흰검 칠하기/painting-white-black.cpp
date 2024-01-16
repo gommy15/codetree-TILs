f3da695eff1db2a542cf1bb14116185b6d0c7dda
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-16 10:29:20 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240116/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+#define MAX_AR 200000
+#define OFFSET 100000
+
+int n, x;
+char dir;
+int black[MAX_AR+1], white[MAX_AR+1], arr[MAX_AR+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    int now = OFFSET;
+    for(int i=0; i<n; i++) {
+        cin >> x>>dir;
+
+        if(dir == 'L') {
+            while(x--) {
+                arr[now] = 1;
+                white[now]++;
+                if(x) now--;
+            }
+        }
+        else {
+            // x칸 오른쪽으로 칠합니다.
+            while(x--) {
+                arr[now] = 2;
+                black[now]++;
+                if(x) now++;
+            }
+        }
+
+        // if(dir == 'L') {
+        //     for(int j = now; j>=now-x; j--) {
+        //         white[j]++;
+        //         arr[j] = 1;
+        //     }
+        //     now -= x;
+        // } else if (dir == 'R') {
+        //     for(int j=now; j<=now+x; j++) {
+        //         black[j]++;
+        //         arr[j] = 2;
+        //     }
+        //     now += x;
+        // }
+    }
+
+    int bl_num=0, wh_num=0, gr_num=0;
+    for(int i=0; i<=MAX_AR; i++) {
+        if(black[i] >= 2 && white[i] >=2) gr_num++;
+        else if(arr[i] == 1) wh_num++;
+        else if(arr[i] == 2) bl_num++; 
+    }
+
+    cout << wh_num << ' ' << bl_num << ' ' << gr_num;
+    return 0;
+}
