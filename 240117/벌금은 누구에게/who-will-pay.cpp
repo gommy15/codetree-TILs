8cabe8289f80302656a0c3fd8521840785090183
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-17 14:58:48 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 100
+
+int n, m, k;
+int student[MAX_N+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m>>k;
+
+    int stu_num;
+    int first_stu = -1;
+    for(int i=0; i<m; i++) {
+        cin >> stu_num;
+        student[stu_num]++;
+
+        for(int j=1; j<=n; j++) {
+            if(student[j] >= k) {
+                first_stu = j;
+                break;
+            }
+        }
+        if(first_stu != -1) break;
+    }
+
+    cout << first_stu;
+    return 0;
+}
