ae66982237c29e5b31271fdbcda21f137900bd9a
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-17 14:49:06 +0900

    Title: 만나는 그 순간, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240117/만나는 그 순간/the-moment-we-meet.cpp
+++ 240117/만나는 그 순간/the-moment-we-meet.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-#define MAX_AR 1000
+#define MAX_AR 1000000
 
 int n, m;
-int arr_A[MAX_AR], arr_B[MAX_AR];
+int arr_A[MAX_AR+1], arr_B[MAX_AR+1];
 char d;
 int t;
 
@@ -17,12 +17,16 @@ int main() {
 
         if(d == 'L') d_num = -1;
         else d_num = 1;
-        for(int i=idx; i<idx+t; i++) {
+        for(int i=idx; i<=idx+t; i++) {
             arr_A[i] = now;
             now += d_num;
+            // cout << arr_A[i] << ' ';
         }
         idx += t;
     }
+
+    // cout << '\n';
+    int min_idx = idx;
 
     now = 0, idx = 0;
     for(int i=0; i<m; i++) {
@@ -30,15 +34,19 @@ int main() {
 
         if(d == 'L') d_num = -1;
         else d_num = 1;
-        for(int i=idx; i<idx+t; i++) {
+        for(int i=idx; i<=idx+t; i++) {
             arr_B[i] = now;
             now += d_num;
+            // cout << arr_B[i] << ' ';
         }
         idx += t;
     }
 
+    // cout << '\n';
+    min_idx = min(min_idx, idx);
+
     int meet_time = -1;
-    for(int i=1; i<MAX_AR; i++) {
+    for(int i=1; i<=min_idx; i++) {
         if(arr_A[i] == arr_B[i]) {
             meet_time = i;
             break;
