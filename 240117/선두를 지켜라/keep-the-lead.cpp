abbe255700b6fd9ae3160ed2749e991cd51ba19a
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-17 15:17:35 +0900

    Title: 선두를 지켜라, Time: 15ms, Memory: 6MB, Status: Passed - CodeTree

---
+++ 240117/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+#define MAX_AR 1000000
+
+int n, m, v, t;
+int now_A[MAX_AR+1], now_B[MAX_AR+1];
+int checked[MAX_AR+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+
+    int now = 1;
+    for(int i=0; i<n; i++) {
+        cin >> v>>t;
+
+        while(t--) {
+            now_A[now] = now_A[now-1] + v;
+            now++;
+        }
+    }
+
+    now = 1;
+    for(int i=0; i<m; i++) {
+        cin >> v>>t;
+
+        while(t--) {
+            now_B[now] = now_B[now-1] + v;
+            now++;
+        }
+    }
+
+    int leader = 0;
+    for(int i=1; i<=now; i++) {
+        if(now_A[i] > now_B[i]) {
+            checked[i] = 1;
+            leader = 1;
+        } else if (now_A[i] < now_B[i]) {
+            checked[i] = 2;
+            leader = 2;
+        } else {
+            checked[i] = leader; 
+        }
+    }
+
+    int cnt = 0;
+    for(int i=1; i<now; i++) {
+        if(checked[i] != checked[i+1]) cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
