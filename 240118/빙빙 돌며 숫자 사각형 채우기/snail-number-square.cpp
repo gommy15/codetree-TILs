28d200b087400406a59d58ff488960fd9d2fde71
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-18 15:57:54 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240118/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#define MAX_NM 100
+
+int arr[MAX_NM][MAX_NM];
+int n, m;
+int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+
+bool InRange(int x, int y) {
+    return 0<=x && x<n && 0<=y && y<m;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >>n>>m;
+    int dir_num = 0, num = 1;
+    int x = 0, y = 0;
+    
+    arr[0][0] = 1;
+    for(int i=2; i<=n*m; i++) {
+        int nx = x+dx[dir_num], ny = y+dy[dir_num];
+
+        if(!InRange(nx, ny) || arr[nx][ny] != 0) {
+            dir_num = (dir_num+1)%4;
+        }
+
+        x += dx[dir_num];
+        y += dy[dir_num];
+        arr[x][y] = i;
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
