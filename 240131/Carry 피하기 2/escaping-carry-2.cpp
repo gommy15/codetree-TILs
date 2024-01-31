e77278ba1aca3a76a1170bdd22f2b5297c37a51e
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-01-31 18:09:22 +0900

    Title: Carry 피하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/Carry 피하기 2/escaping-carry-2.cpp
+++ 240131/Carry 피하기 2/escaping-carry-2.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int n;
 int arr[MAX_N];
 
+// carry를 찾으면 true, 아니면 false
 bool FindCarry(int a, int b, int c) {
     int sum = a+b+c;
     int na, nb, nc;
@@ -33,17 +34,20 @@ int main() {
         cin >> arr[i];
     }
 
-    int max_sum = INT_MIN;
-    bool exist = false;
+    int max_sum = -1;
+    // int max_sum = INT_MIN;
+    //모두 캐리만 나오는 조합들만 있으면
+    // bool exist = false;
 
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             if(i != j) {
                 for(int k=0; k<n; k++) {
                     if(k != i && k != j) {
+                        // carry를 찾지 못하면
                         if(!FindCarry(arr[i], arr[j], arr[k])) {
                             max_sum = max(max_sum, arr[i]+arr[j]+arr[k]);
-                            exist = true;
+                            // exist = true;
                         }
                     }
                 }
@@ -51,8 +55,11 @@ int main() {
         }
     }
 
-    if(exist) cout << max_sum;
-    else cout << -1;
+    // 양수 조합들의 합이기 떄문에 음수값이 나올 수 없다
+    // max_sum의 초기값을 -1로 한다면 굳이 변수를 하나 더 만들어 검증을 거칠 필요가 없다.
+    cout << max_sum;
+    // if(exist) cout << max_sum;
+    // else cout << -1;
 
     return 0;
 }
