7d4837bcea920a49365188d49ccaaa7c1fb9e84d
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-01 14:26:27 +0900

    Title: 숨은 단어 찾기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/숨은 단어 찾기 2/find-hidden-words-2.cpp
+++ 240201/숨은 단어 찾기 2/find-hidden-words-2.cpp
@@ -6,6 +6,10 @@ int n, m;
 int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 char arr[MAX_NM][MAX_NM];
 
+bool InRange(int x, int y) {
+    return 0 <= x && x <n && 0 <= y && y < m;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n>>m;
@@ -21,7 +25,8 @@ int main() {
             if(arr[i][j] == 'L') {
                 for(int k=0; k<8; k++) {
                     int x = i+dx[k], y = j+dy[k];
-                    if(arr[x][y] == 'E' && arr[x+dx[k]][y+dy[k]] == 'E') cnt++;
+                    // 범위안에 들어있는지 확인하기!!
+                    if(InRange(x, y) && InRange(x+dx[k], y+dy[k]) && arr[x][y] == 'E' && arr[x+dx[k]][y+dy[k]] == 'E') cnt++;
                 }
             }
         }
