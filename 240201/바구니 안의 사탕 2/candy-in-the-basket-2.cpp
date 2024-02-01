4ba35c7d194b45a05fbbf76d8970ab4fc06a5d86
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-01 16:04:27 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX_N   100
+
+int n, k;
+int arr[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>k;
+    int idx, num;
+    for(int i=0; i<n; i++) {
+        cin >> num>>idx;
+        arr[idx] = num;
+    }
+
+    int max_sum = 0;
+
+    for(int i=0; i<=MAX_N-2*k-1; i++) {
+        int sum = 0;
+        for(int j=i; j<=i+2*k; j++) {
+            sum += arr[j];
+        }
+        max_sum = max(max_sum, sum);
+    }
+
+    cout << max_sum;
+    return 0;
+}
