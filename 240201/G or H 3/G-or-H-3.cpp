870c96e200b49543591fcb53e0d901557970b923
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-01 15:25:51 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#define MAX_ARR 10000
+
+int n, k;
+int arr[MAX_ARR];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>k;
+
+    int idx;
+    char alpa;
+
+    for(int i=0; i<n; i++) {
+        cin >> idx>>alpa;
+        if(alpa == 'G') arr[idx-1] = 1;
+        else arr[idx-1] = 2;
+    }
+
+    int max_sum = 0;
+    for(int i=0; i<=n-k; i++) {
+        int sum = 0;
+        for(int j=i; j<=i+k; j++) {
+            sum += arr[j];
+        }
+        max_sum = max(max_sum, sum);
+    }
+
+    cout << max_sum;
+    return 0;
+}
