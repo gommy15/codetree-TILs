050fb631de455fca1ef1f06c5fb0c32069b82185
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-01 17:31:02 +0900

    Title: 두 가지로 열리는 자물쇠, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240201/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 240201/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -11,8 +11,17 @@ int main() {
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
             for(int k=1; k<=n; k++) {
-                if((abs(a1-i) <= 2 || abs(a1+n-i) <= 2) && (abs(b1-j) <= 2 || abs(b1+n-j) <= 2)  && (abs(c1-k) <= 2 || abs(c1+n-k) <= 2)) cnt++;
-                else if((abs(a2-i) <= 2 || abs(a2+n-i) <= 2) && (abs(b2-j) <= 2 || abs(b2+n-j) <= 2)  && (abs(c2-k) <= 2 || abs(c2+n-k) <= 2)) cnt++;
+                // if((abs(a1-i) <= 2 || abs(a1+n-i) <= 2) && (abs(b1-j) <= 2 || abs(b1+n-j) <= 2)  && (abs(c1-k) <= 2 || abs(c1+n-k) <= 2)) cnt++;
+                // else if((abs(a2-i) <= 2 || abs(a2+n-i) <= 2) && (abs(b2-j) <= 2 || abs(b2+n-j) <= 2)  && (abs(c2-k) <= 2 || abs(c2+n-k) <= 2)) cnt++;
+                 // 모든 자리가 주어진 첫 조합과의 거리가 2 이내인지 확인합니다.
+                if((abs(a - i) <= 2 || abs(a - i) >= n - 2) && (abs(b - j) <= 2 || abs(b - j) >= n - 2) && 
+                   (abs(c - k) <= 2 || abs(c - k) >= n - 2))
+                    cnt++;
+				
+				// 모든 자리가 주어진 두번째 조합과의 거리가 2 이내인지 확인합니다.
+                else if((abs(a2 - i) <= 2 || abs(a2 - i) >= n - 2) && (abs(b2 - j) <= 2 || abs(b2 - j) >= n - 2) && 
+                   (abs(c2 - k) <= 2 || abs(c2 - k) >= n - 2))
+                    cnt++;
             }
         }
     }
