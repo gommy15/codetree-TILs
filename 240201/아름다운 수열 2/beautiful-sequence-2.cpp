6160678d3579b1a71ab8e632cbfa769ad02abfc4
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-01 15:56:12 +0900

    Title: 아름다운 수열 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 240201/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 #define MAX_NM 100
 
 int n, m;
-int arr_a[MAX_NM], arr_b[MAX_NM];
+int arr_a[MAX_NM], arr_b[MAX_NM], tmp[MAX_NM];
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -15,19 +16,26 @@ int main() {
         cin >> arr_b[i];
     }
 
+    // sort를 하면 아름다운 수열을 정렬하여 쉽게 확인할 수 있다.
+    sort(arr_b, arr_b+m);
     int cnt = 0;
 
     for(int i=0; i<=n-m; i++) {
-        int num = 0;
-        for(int j=i; j<i+m; j++) {
-            bool exist = false;
-            for(int k=0; k<m; k++) {
-                if(arr_a[j] == arr_b[k]) exist = true;
-            }
-            if(!exist) break;
-            else num++;
+        //임의의 배열을 두어 섞이지 않고 배열을 비교할 수 있도록 한다.
+        for(int j=0; j<m; j++) {
+            tmp[j] = arr_a[i+j];
         }
-        if(num == m) cnt++;
+
+        sort(tmp, tmp+m);
+
+        bool same = true;
+        for(int j=0; j<m; j++) {
+            if(tmp[j] != arr_b[j]) {
+                same = false;
+                break;
+            }
+        }
+        if(same) cnt++;
     }
 
     cout << cnt;
