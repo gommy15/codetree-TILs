b87731e842bb8402796f9b20f2654eadf89388e5
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-01 17:23:55 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int n, a1, b1, c1, a2, b2, c2;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>a1>>b1>>c1>>a2>>b2>>c2;
+
+    int cnt = 0;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            for(int k=1; k<=n; k++) {
+                if((abs(a1-i) <= 2 || abs(a1+n-i) <= 2) && (abs(b1-j) <= 2 || abs(b1+n-j) <= 2)  && (abs(c1-k) <= 2 || abs(c1+n-k) <= 2)) cnt++;
+                else if((abs(a2-i) <= 2 || abs(a2+n-i) <= 2) && (abs(b2-j) <= 2 || abs(b2+n-j) <= 2)  && (abs(c2-k) <= 2 || abs(c2+n-k) <= 2)) cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
