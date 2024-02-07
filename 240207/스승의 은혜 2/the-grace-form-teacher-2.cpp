1eb5504bbfb9ede8a25d677d8a20b1fe181f617f
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-07 11:29:37 +0900

    Title: 스승의 은혜 2, Time: 42ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX_N 1000
+
+int n, b;
+int p[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> b;
+    // int pres;
+    for(int i=0; i<n; i++) {
+        cin >> p[i];
+    }
+
+    int max_student = 0;
+    //반값을 적용할 학생 선택
+    for(int i=0; i<n; i++) {
+        int tmp[MAX_N] = {};
+
+        for(int j=0; j<n; j++) {
+            if(i==j) tmp[j] = (p[j]/2);
+            else tmp[j] = p[j];
+        }
+        // 가장 예산이 작은 학생부터 채우는 것이 가장 많은 학생에게 선물을 줄 수 있는 방법임!
+        sort(tmp, tmp+n);
+
+        int budget = 0;
+        int student = 0;
+        
+        for(int j=0; j<n; j++) {
+            // if(budget + tmp[j] > b) break;
+
+            // budget += tmp[j];
+            // student++;
+
+            budget += tmp[j];
+
+            if(budget <= b) student++;
+            else break;
+        }
+
+        max_student = max(max_student, student);
+    }
+
+    cout << max_student;
+    return 0;
+}
