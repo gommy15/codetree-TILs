5f6e43e07b09912b780203c62a926cbaa3c64c6f
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-02-16 16:21:07 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240216/등차수열/arithmetic-sequence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 100
+#define MAX_NUM 100
+
+int n;
+int nums[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    int max_cnt = 0;
+    for(int k=1; k<=MAX_NUM; k++) {
+        int cnt = 0;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                if((nums[i]+nums[j]) == (2*k)) cnt++;
+            }
+        }
+        max_cnt = max(max_cnt, cnt);
+    }
+
+    cout << max_cnt;
+    return 0;
+}
