3ab4a67f42d34ecc00fd25bc4049694a19f1e375
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-04 21:11:16 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240304/두 선분/two-lines.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int x1, x2, x3, x4;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> x1>>x2>>x3>>x4;
+
+    if((x1 <= x3 && x3 <= x2) || (x1 <= x4 && x4 <= x2)) {
+        cout << "intersecting" << endl;
+    } else {
+        cout << "nointersecting" << endl;
+    }
+    return 0;
+}
