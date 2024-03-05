eb7db230b470a70c798b9fa514a72e3953f59528
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-05 19:52:55 +0900

    Title: X 달리기, Time: 1057ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240305/X 달리기/x-run.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int x;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> x;
+    int t = 0, v = 0, len = 0;
+
+    while(true) {
+        if(len == x) break;
+        if(len <= x/2) {
+            v++;
+        } else {
+            if(v != 1) v--;
+        }
+
+        len += v;
+        t++;
+        // cout << t << ' ' << v << ' ' << len << endl;
+    }
+
+    cout << t;
+
+    return 0;
+}
