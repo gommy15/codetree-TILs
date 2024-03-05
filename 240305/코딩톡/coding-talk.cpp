c5517e129348b444dfb5b329af44bcb9a925092b
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-05 19:43:06 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/코딩톡/coding-talk.cpp
+++ 240305/코딩톡/coding-talk.cpp
@@ -7,25 +7,6 @@ using namespace std;
 int n, m, p;
 char c[MAX_M];
 int u[MAX_M];
-int member[MAX_N];
-// pair<char, int> message[MAX_M+1];
-
-void FindMember() {
-    if(u[p-1] == 0) {
-        cout << ' ';
-    } else {
-        for(int i=p-1; i<m; i++) {
-           member[(int)(c[i]-'A')] = 1;
-        }   
-
-        for(int i=0; i<n; i++) {
-            if(member[i] == 0) {
-                char mem = 'A'+i;
-                cout << mem << ' ';
-            }
-        }
-    }
-}
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -35,7 +16,24 @@ int main() {
         cin >> c[i]>>u[i];
     }
 
-    FindMember();
+    // 모두 읽은 채팅이라면 읽지 않은 사람 없음
+    if(u[p-1] == 0) return 0;
+
+    // 각 사람에 대해 채팅을 읽었는지 확인
+    for(int i=0; i<n; i++) {
+        char member = 'A' + i;
+        bool read = false;
+
+        for(int j=0; j<m; j++) {
+            // p번 메시지를 읽은 사람 수와 같거나 많은 채팅을 기준으로 단 한번이라도 채팅을 쳤다면 확실히 채팅을 읽음
+            if(u[j] >= u[p-1] && c[j] == member) read = true;
+        }
+
+        if(read == false)
+            cout << member << ' ';
+    }
+
+    
     
     return 0;
 }
