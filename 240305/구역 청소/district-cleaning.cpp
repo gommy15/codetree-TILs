779b57c0020d3a7731ffe3496db43e26e9bc48ca
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-05 16:27:17 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/구역 청소/district-cleaning.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    cin >> a>>b>>c>>d;
+
+    if(b<c || d<a) {
+        cout << (d-c) + (b-a) << endl;
+    } else {
+        int tmp[101] = {};
+        for(int i=a; i<b; i++) {
+            tmp[i] = 1;
+        }
+        for(int i=c; i<d; i++) {
+            tmp[i] = 1;
+        }
+
+        int cnt = 0;
+        for(int i=0; i<101; i++) {
+            if(tmp[i] == 1) cnt++;
+        }
+
+        cout << cnt;
+    }
+    return 0;
+}
