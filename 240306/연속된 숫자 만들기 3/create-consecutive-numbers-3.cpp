77f1cb4c84798d152043f6d362bde7f8e8137795
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-06 21:18:13 +0900

    Title: 연속된 숫자 만들기 3, Time: 1014ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240306/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 240306/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -12,6 +12,10 @@ int main() {
     sort(arr, arr+3);
 
     int cnt = 0;
+    if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) {
+        cout << 0;
+        return 0;
+    }
     if((arr[1]-arr[0]) > (arr[2]-arr[1])) {
         while(true) {
             if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) break;
