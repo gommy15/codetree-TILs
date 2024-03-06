8ba5ae017821246c943ee922fb7e0670a3637132
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-06 18:08:03 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240306/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -14,7 +14,7 @@ int main() {
     // 주어진 숫자가 연속한 경우
     if((arr[1]-arr[0]) == 1 && (arr[2]-arr[1]) == 1) cout << 0;
     // 숫자간 간격이 2정도 떨어진 그룹이 있는 경우
-    else if((arr[1]-arr[0]) == 2 || (arr[2]-arr[1]) == 1) cout << 1;
+    else if((arr[1]-arr[0]) == 2 || (arr[2]-arr[1]) == 2) cout << 1;
     // 숫자간 간격이 모두 3이상 떨어진 경우
     else cout << 2;
     
