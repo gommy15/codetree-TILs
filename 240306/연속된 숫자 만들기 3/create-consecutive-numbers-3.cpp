a20c580d44d570f246e7edacb9ed9766a36f05a3
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-06 21:23:41 +0900

    Title: 연속된 숫자 만들기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 240306/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -11,27 +11,41 @@ int main() {
     }
     sort(arr, arr+3);
 
-    int cnt = 0;
     if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) {
         cout << 0;
         return 0;
     }
+
+    int max_move = 0;
+    // 최대 이동 횟수는 가운데 사람과 가장 멀리 떨어져 있는 쪽으로 이동하는 경우이며
+    // 오른쪽이 멀다면 (오른쪽 끝 사람과 가운데 사람과의 거리) - 1이고
+    // 왼쪽이 멀다면 (왼쪽 끝 사람과 가운데 사람과의 거리) -1 이다.
+    int move;
     if((arr[1]-arr[0]) > (arr[2]-arr[1])) {
-        while(true) {
-            if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) break;
-            arr[2] = arr[1]-1;
-            cnt++;
-            sort(arr, arr+3);
-        }
+        move = arr[1]-arr[0]-1;
     } else {
-        while(true) {
-            if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) break;
-            arr[0] = arr[1]+1;
-            cnt++;
-            sort(arr, arr+3);
-        }
+        move = arr[2]-arr[1]-1;
     }
 
-    cout << cnt;
+    cout << move;
+
+
+    // if((arr[1]-arr[0]) > (arr[2]-arr[1])) {
+    //     while(true) {
+    //         if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) break;
+    //         arr[2] = arr[1]-1;
+    //         cnt++;
+    //         sort(arr, arr+3);
+    //     }
+    // } else {
+    //     while(true) {
+    //         if(arr[0]+1 == arr[1] && arr[1]+1 == arr[2]) break;
+    //         arr[0] = arr[1]+1;
+    //         cnt++;
+    //         sort(arr, arr+3);
+    //     }
+    // }
+
+    // cout << cnt;
     return 0;
 }
