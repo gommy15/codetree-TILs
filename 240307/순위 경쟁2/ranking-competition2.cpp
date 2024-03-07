984f02eca8df59c102f54555793df0c734a34e99
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-07 14:31:10 +0900

    Title: 순위 경쟁2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240307/순위 경쟁2/ranking-competition2.cpp
+++ 240307/순위 경쟁2/ranking-competition2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define MAX_N 100
 
 int n, a=0, b=0;
-int wins[MAX_N];
+int wins[MAX_N+1];
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -21,8 +21,8 @@ int main() {
         else wins[i] = 0;
     }
 
-    int cnt = 1;
-    for(int i=1; i<n; i++) {
+    int cnt = 0;
+    for(int i=0; i<n; i++) {
         if(wins[i-1] != wins[i]) cnt++;
     }
 
