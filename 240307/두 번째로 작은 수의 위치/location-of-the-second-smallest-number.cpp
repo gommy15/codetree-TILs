ecbc88650657bed7f3999161a849ed6ef482ef96
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-07 14:43:16 +0900

    Title: 두 번째로 작은 수의 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240307/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
+++ 240307/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -4,28 +4,39 @@ using namespace std;
 #define MAX_N 100
 
 int n;
-int arr[MAX_N], tmp[MAX_N];
+int arr[MAX_N], s_arr[MAX_N];
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
     for(int i=0; i<n; i++) {
         cin >> arr[i];
-        tmp[i] = arr[i];
+        s_arr[i] = arr[i];
     }
 
-    sort(tmp, tmp+n);
+    sort(s_arr, s_arr+n);
+    int tmp[MAX_N] = {};
+    int cnt = 0;
+    for(int i=0; i<n; i++) {
+        if(s_arr[i-1] != s_arr[i])
+            tmp[cnt++] = s_arr[i];
+    }
 
-    if(tmp[0] == tmp[1] || tmp[1] == tmp[2]) {
+    if(cnt == 1) {
         cout << -1;
         return 0;
     }
 
+    int sec_cnt = 0;
+    int ans = 0;
     for(int i=0; i<n; i++) {
         if(arr[i] == tmp[1]) {
-            cout << i+1;
-            break;
+            sec_cnt++;
+            ans = i+1;
         }
     }
+
+    if(sec_cnt > 1) cout << -1;
+    else cout << ans;
     return 0;
 }
