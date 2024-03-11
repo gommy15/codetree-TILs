b4b341d816fbed5761e4f4ec71986c0f1497b88a
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-11 15:54:07 +0900

    Title: A, B, C 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/A, B, C 찾기/finding-a-b-c.cpp
+++ 240311/A, B, C 찾기/finding-a-b-c.cpp
@@ -11,12 +11,21 @@ int main() {
     }
 
     sort(nums, nums+7);
-    int abc = nums[6];
-    int bc = nums[5];
-    int a = abc - bc;
-    int ac = nums[4];
-    int c = ac - a;
-    int b = bc - c;
+    // 2차 풀이
+    // 오름차순으로 정렬했을 때, 가장 작은 수는 A, 두번째로 작은 수는 항상 B이다.
+    int a = nums[0];
+    int b = nums[1];
+
+    // 오름차순으로 정렬했을 때, 가장 큰 수는 항상 a+b+c이다.
+    int c = nums[6]-a-b;
+
+    // 1차 풀이
+    // int abc = nums[6];
+    // int bc = nums[5];
+    // int a = abc - bc;
+    // int ac = nums[4];
+    // int c = ac - a;
+    // int b = bc - c;
 
     cout << a << ' ' << b << ' ' << c;
     return 0;
