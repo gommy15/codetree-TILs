aa745ca372ead1e9fd383dbdbab3ca24ca736adf
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-11 14:48:39 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240311/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 240311/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -8,12 +8,18 @@ int arr[MAX_N+1];
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n>>m;
+    int cnt = 0;
     for(int i=1; i<=n; i++) {
         cin >> arr[i];
+        if(arr[i] == 1) cnt++;
     }
 
+    if(m == 0) {
+        cout << cnt;
+        return 0;
+    }
     int area = 2*m+1;
-    int cnt = n/area;
+    cnt = n/area;
     if(n%area != 0) cnt++;
 
     cout << cnt;
