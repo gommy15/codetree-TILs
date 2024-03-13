9249ef1e61413930ff9e8e8c4f172f1fa6cfecf5
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-13 14:54:16 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX_N 20
+
+int n, m;
+int arr[MAX_N][MAX_N];
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+bool InRange(int x, int y) {
+    return 0<=x && x<n && 0<=y && y<n;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int k=1;
+    int ans = 1;
+    while(true) {
+        int max_cnt = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                int cnt = 0;
+                for(int l=0; l<4; l++) {
+                    int nx = i+dx[l]*k, ny = j+dy[l]*k;
+                    if(!InRange(nx, ny)) continue;
+
+                    if(arr[nx][ny] == 1) cnt++;
+                }
+
+                if(k > 1) {
+                    int nx = i-(k-1), ny = j-(k-1);
+                    for(int a=nx; a<=nx+k; a++) {
+                        for(int b=ny; b<=ny+k; b++) {
+                            if(!InRange(a, b)) continue;
+
+                            if(arr[a][b] == 1) cnt++;
+                        }
+                    }
+                }
+
+                max_cnt = max(max_cnt, cnt);
+            }
+        }
+
+        if(max_cnt*m > (k*k + (k+1)*(k+1)))
+            ans = max(ans, max_cnt);
+        else
+            break;
+
+        k++;
+    }
+
+    cout << ans;
+    return 0;
+}
