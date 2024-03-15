06d28d41693a7ebb9802fd1ff613d04ced8a5ef1
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-15 10:58:02 +0900

    Title: 양수 직사각형의 최대 크기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240315/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 240315/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -9,7 +9,7 @@ int grid[MAX_NM][MAX_NM];
 bool CheckRect(int x1, int y1, int x2, int y2) {
     for(int i=x1; i<=x2; i++) {
         for(int j=y1; j<=y2; j++) {
-            if(grid[i][j] < 0) return false;
+            if(grid[i][j] <= 0) return false;
         }
     }
     return true;
