6681dadf10c9bea215dda7fe3be6f2c3463f6cfe
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-22 19:13:57 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 20
+#define DIR_NUM 4
+
+int n, m, t, r, c;
+int grid[MAX_N+1][MAX_N+1];
+int grid_ball[MAX_N+1][MAX_N+1];
+int dx[DIR_NUM] = {-1, 1, 0, 0}, dy[DIR_NUM] = {0, 0, -1, 1};
+
+bool InRange(int x, int y) {
+    return 0<x && x<=n && 0<y && y<=n;
+}
+
+void Simulate() {
+    int tmp[MAX_N+1][MAX_N+1] = {};
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            if(grid_ball[i][j] == 1) {
+                int max_num = grid[i][j];
+                int next_x, next_y;
+                for(int k=0; k<DIR_NUM; k++) {
+                    int nx = i+dx[k], ny = j+dy[k];
+                    if(!InRange(nx, ny)) continue;
+
+                    if(max_num < grid[nx][ny]) {
+                        next_x = nx;
+                        next_y = ny;
+                        max_num = grid[nx][ny];
+                    }
+                }
+                if(max_num == grid[i][j]) tmp[i][j]++;
+                else tmp[next_x][next_y]++;
+            }
+        }
+    }
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            if(tmp[i][j] != 1) grid_ball[i][j] = 0;
+            else grid_ball[i][j] = tmp[i][j];
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m>>t;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for(int i=0; i<m; i++) {
+        cin >> r>>c;
+        grid_ball[r][c]++;
+    }
+
+    while(t--) {
+        Simulate();
+    }
+
+    int ans = 0;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            ans += grid_ball[i][j];
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
