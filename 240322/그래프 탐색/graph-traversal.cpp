ddd9ce349495d07e25c04cdffddc615af3766f70
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-22 21:55:01 +0900

    Title: 그래프 탐색, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240322/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX_N 1000
+
+int n, m;
+int vertex_cnt;
+
+vector<int> graph[MAX_N+1];
+bool visited[MAX_N+1];
+
+void DFS(int vertex) {
+    for(int i=0; i<(int)graph[vertex].size(); i++) {
+        int cur_v = graph[vertex][i];
+        if(!visited[cur_v]) {
+            visited[cur_v] = true;
+            vertex_cnt++;
+            DFS(cur_v);
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n>>m;
+
+    int v1, v2;
+    for(int i=0; i<m; i++) {
+        cin >> v1>>v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+    }
+
+    visited[1] = true;
+    DFS(1);
+
+    cout << vertex_cnt;
+    return 0;
+}
