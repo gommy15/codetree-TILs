a9e7b50d771752c1e4ae6295b5f1efbf7e49fbdf
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-03-22 22:17:05 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240322/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -8,7 +8,7 @@ int visited[MAX_NM][MAX_NM];
 int dx[2] = {1, 0}, dy[2] = {0, 1};
 
 bool InRange(int x, int y) {
-    return 0<=x && x<n && 0<=y && y<n;
+    return 0<=x && x<n && 0<=y && y<m;
 }
 
 bool CanGo(int x, int y) {
@@ -42,7 +42,7 @@ int main() {
     visited[0][0] = 1;
     DFS(0, 0);
 
-    if(visited[0][0] && visited[n-1][n-1]) cout << 1;
+    if(visited[0][0] && visited[n-1][m-1]) cout << 1;
     else cout << 0;
 
     return 0;
