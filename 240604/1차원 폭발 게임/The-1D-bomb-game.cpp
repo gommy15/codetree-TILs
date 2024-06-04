3d3a752a00c2121ed8d332e6713a854f85b2e403
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-06-04 14:06:34 +0900

    Title: 1차원 폭발 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240604/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 100
+
+int n, m, ans;
+int bombs[MAX_N];
+
+int CheckBombs(int x) {
+    int cnt = 1;
+    for(int i=x+1; i<n; i++) {
+        if(bombs[x] != bombs[i]) break;
+
+        cnt++;
+    }
+
+    return cnt;
+}
+
+void DeleteBumbs(int x, int cnt) {
+    for(int i=x; i<x+cnt; i++) {
+        bombs[i] = 0;
+    }
+}
+
+bool Simulate() {
+    int exist = false;
+    for(int i=0; i<n-1; i++) {
+        if(bombs[i] != 0 && bombs[i] == bombs[i+1]) {
+            int nums = CheckBombs(i);
+            if(nums >= m) {
+                DeleteBumbs(i, nums);
+                exist = true;
+            }
+        }
+    }
+
+    if(!exist) return exist;
+
+    int tmp[MAX_N] = {};
+    int cnt = 0;
+    for(int i=0; i<n; i++) {
+        if(bombs[i] != 0)
+            tmp[cnt++] = bombs[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        bombs[i] = tmp[i];
+    }
+
+    ans = cnt;
+    return true;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >>n>>m;
+    for(int i=0; i<n; i++) {
+        cin >> bombs[i];
+    }
+
+    while(Simulate()) {};
+
+    cout << ans << '\n';
+    for(int i=0; i<ans; i++) {
+        cout << bombs[i] << '\n';
+    }
+    
+    return 0;
+}
