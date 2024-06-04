1325438629f76d1fb5d01e6986cd7c25f771abb5
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-06-04 15:33:58 +0900

    Title: 2차원 폭발 게임, Time: 25ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240604/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 100
+
+int n, m, k;
+int grid[MAX_N][MAX_N], next_grid[MAX_N][MAX_N];
+
+int CntBumbs(int x, int y) {
+    int cnt = 1;
+    for(int i=x+1; i<n; i++) {
+        if(grid[x][y] != grid[i][y]) break;
+
+        cnt++;
+    }
+
+    return cnt;
+}
+
+void Bomb(int x, int y, int cnt) {
+    for(int i=x; i<x+cnt; i++) {
+        grid[i][y] = 0;
+    }
+}
+
+void Drop() {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            next_grid[i][j] = 0;
+        }
+    }
+
+    for(int col=0; col<n; col++) {
+        int cnt = n-1;
+        for(int row=n-1; row>=0; row--) {
+            if(grid[row][col] != 0) {
+                next_grid[cnt--][col] = grid[row][col];
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            grid[i][j] = next_grid[i][j];
+        }
+    }
+}
+
+void Rotate() {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            next_grid[i][j] = 0;
+        }
+    }
+
+    for(int i=n-1; i>=0; i--) {
+        for(int j=n-1;j>=0; j--) {
+            next_grid[j][n-i-1] = grid[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            grid[i][j] = next_grid[i][j];
+        }
+    }
+}
+
+void Simulate() {
+    for(int col=0; col<n; col++) {
+        for(int row=0; row<n; row++) {
+            if(grid[row][col] != 0) {
+                int cnt = CntBumbs(row, col);
+                if(cnt >= m) {
+                    Bomb(row, col, cnt);
+                }
+            }
+        }
+    }
+
+    Drop();
+
+    Rotate();
+    Drop();
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m>>k;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    while(k--) {
+        Simulate();
+
+        // cout << "*******************************\n";
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         cout << grid[i][j] << ' ';
+        //     }
+        //     cout << '\n';
+        // }
+
+    }
+
+    for(int col=0; col<n; col++) {
+        for(int row=0; row<n; row++) {
+            if(grid[row][col] != 0) {
+                int cnt = CntBumbs(row, col);
+                if(cnt >= m) {
+                    Bomb(row, col, cnt);
+                }
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(grid[i][j] != 0) ans++;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
