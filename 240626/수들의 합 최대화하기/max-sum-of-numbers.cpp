cf3dd38419c607eac9aab4fb4f986ed386cc8e5c
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-06-26 19:36:33 +0900

    Title: 수들의 합 최대화하기, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

--- 240626/수들의 합 최대화하기/max-sum-of-numbers.cpp
+++ 240626/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -8,41 +8,47 @@ using namespace std;
 int n;
 int grid[MAX_N][MAX_N];
 
-bool vx[MAX_N], vy[MAX_N];
-vector<pair<int, int>> selected_pos;
+bool visited[MAX_N];
+// vector<pair<int, int>> selected_pos;
 int ans = 0;
 
-int Calc() {
-    int x, y;
-    int sum = 0;
-    for(int i=0; i<selected_pos.size(); i++) {
-        tie(x, y) = selected_pos[i];
-        sum += grid[x][y];
-    }
+// int Calc() {
+//     int x, y;
+//     int sum = 0;
+//     for(int i=0; i<selected_pos.size(); i++) {
+//         tie(x, y) = selected_pos[i];
+//         sum += grid[x][y];
+//     }
 
-    return sum;
-}
+//     return sum;
+// }
 
-void FindMaxNum(int cnt) {
-    if(cnt == n) {
-        ans = max(ans, Calc());
+void FindMaxNum(int row, int sum) {
+    if(row == n) {
+        ans = max(ans, sum);
         return;
     }
 
     for(int i=0; i<n; i++) {
-        if(vx[i]) continue;
-        for(int j=0; j<n; j++) {
-            if(vy[j]) continue;
+        if(visited[i]) continue;
 
-            selected_pos.push_back(make_pair(i, j));
-            vx[i] = true;
-            vy[j] = true;
-            FindMaxNum(cnt+1);
-            selected_pos.pop_back();
-            vx[i] = false;
-            vy[j] = false;
-        }
+        visited[i] = true;
+        FindMaxNum(row+1, sum + grid[row][i]);
+        visited[i] = false;
     }
+
+    // for(int i=0; i<n; i++) {
+    //     if(vx[i]) continue;
+    //     for(int j=0; j<n; j++) {
+    //         if(vy[j]) continue;
+
+    //         selected_pos.push_back(make_pair(i, j));
+    //         vx[i] = vy[j] = true;
+    //         FindMaxNum(cnt+1);
+    //         selected_pos.pop_back();
+    //         vx[i] = vy[j] = false;
+    //     }
+    // }
 }
 
 int main() {
@@ -55,7 +61,7 @@ int main() {
         }
     }
 
-    FindMaxNum(0);
+    FindMaxNum(0, 0);
 
     cout << ans;
     return 0;
