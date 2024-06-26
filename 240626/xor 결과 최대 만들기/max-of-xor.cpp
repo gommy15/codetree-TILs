b543dd7d7b37912b94c741967b0af16e091769fb
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-06-26 16:50:03 +0900

    Title: xor 결과 최대 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240626/xor 결과 최대 만들기/max-of-xor.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define MAX_N 20
+
+int n, m, ans;
+int nums[MAX_N];
+vector<int> cnums;
+
+int Calc() {
+    int result = 1;
+    for(int i=0; i<cnums.size(); i++) {
+        result ^= cnums[i];
+    }
+
+    return result;
+}
+
+void ChooseNum(int cnt, int idx) {
+    if(cnt == m) {
+        ans = max(ans, Calc());
+        return;
+    }
+
+    for(int i=idx; i<n; i++) {
+        cnums.push_back(nums[i]);
+        ChooseNum(cnt+1, i+1);
+        cnums.pop_back();
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    ChooseNum(0, 0);
+
+    cout << ans;
+
+    return 0;
+}
