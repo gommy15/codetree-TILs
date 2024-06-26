60c910c82d2c4e70f2be3088088bca34b8560bf1
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-06-26 19:26:10 +0900

    Title: 수들의 합 최대화하기, Time: 1045ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240626/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+#define MAX_N 10
+
+int n;
+int grid[MAX_N][MAX_N];
+
+bool vx[MAX_N], vy[MAX_N];
+vector<pair<int, int>> selected_pos;
+int ans = 0;
+
+int Calc() {
+    int x, y;
+    int sum = 0;
+    for(int i=0; i<selected_pos.size(); i++) {
+        tie(x, y) = selected_pos[i];
+        sum += grid[x][y];
+    }
+
+    return sum;
+}
+
+void FindMaxNum(int cnt) {
+    if(cnt == n) {
+        ans = max(ans, Calc());
+        return;
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(vx[i] || vy[j]) continue;
+
+            selected_pos.push_back(make_pair(i, j));
+            vx[i] = true;
+            vy[j] = true;
+            FindMaxNum(cnt+1);
+            selected_pos.pop_back();
+            vx[i] = false;
+            vy[j] = false;
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    FindMaxNum(0);
+
+    cout << ans;
+    return 0;
+}
