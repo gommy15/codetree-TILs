2b8634be393d001ac28e574afc0fdc7ba3c3e349
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-06-27 15:37:13 +0900

    Title: 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240627/사각형 채우기/rectangle-fill.cpp
+++ 240627/사각형 채우기/rectangle-fill.cpp
@@ -9,10 +9,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
     
+    dp[0] = 1;
     dp[1] = 1;
-    dp[2] = 2;
 
-    for(int i=3; i<=n; i++) {
+    for(int i=2; i<=n; i++) {
         dp[i] = (dp[i-1]+dp[i-2])%10007;
     }
 
