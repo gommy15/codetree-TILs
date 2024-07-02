11fb9dbb7f997d4f712f3357334d6d4f7968f3a0
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-07-02 20:45:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240702/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+#define MAX_NM 100
+#define DIR_NUM 2
+
+int n, m;
+int grid[MAX_NM][MAX_NM];
+bool visited[MAX_NM][MAX_NM];
+int dx[DIR_NUM] = {1, 0}, dy[DIR_NUM] = {0, 1};
+
+bool InRange(int x, int y) {
+    return 0<=x && x<n && 0<=y && y<m;
+}
+
+bool CanGo(int x, int y) {
+    if(!InRange(x, y)) return false;
+
+    if(visited[x][y]) return false;
+    if(grid[x][y] == 0) return false;
+
+    return true;
+}
+
+void DFS(int x, int y) {
+
+    for(int i=0; i<DIR_NUM; i++) {
+        int nx = x+dx[i], ny = y+dy[i];
+
+        if(CanGo(nx, ny)) {
+            visited[nx][ny] = true;
+            DFS(nx, ny);
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    visited[0][0] = true;
+    DFS(0, 0);
+
+    cout << visited[n-1][m-1];
+    return 0;
+}
