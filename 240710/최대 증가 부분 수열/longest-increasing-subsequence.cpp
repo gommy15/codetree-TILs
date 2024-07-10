2cbe2d3765af6e56fafde681c3b9c47e2b116bf0
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-07-10 14:41:28 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240710/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240710/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -3,19 +3,20 @@ using namespace std;
 #define MAX_N 1000
 
 int n;
-int num[MAX_N], dp[MAX_N];
+int num[MAX_N+1], dp[MAX_N+1];
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
 
-    for(int i=0; i<n; i++) {
+    for(int i=1; i<=n; i++) {
         cin >> num[i];
     }
 
-    dp[0] = 1;
+    dp[0] = 0;
+    num[0] = 0;
 
-    for(int i=1; i<n; i++) {
+    for(int i=1; i<=n; i++) {
         for(int j=0; j<i; j++) {
             if(num[j] < num[i])
                 dp[i] = max(dp[i], dp[j]+1);
@@ -23,7 +24,7 @@ int main() {
     }
 
     int ans = 0;
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<=n; i++) {
         ans = max(ans, dp[i]);
     }
 
