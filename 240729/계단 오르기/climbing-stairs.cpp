bd054daf057d10e9dc110ef576e3efd2c48ee82b
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-07-29 19:53:55 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240729/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 1000
+#define MOD 10007
+
+int n;
+int step[MAX_N+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    step[0] = 1;
+    step[1] = 1;
+    step[2] = 1;
+    step[3] = 1;
+
+    for(int i=4; i<=n; i++) {
+        step[i] = (step[i-2] + step[i-3])%MOD;
+    }
+
+    cout << step[n];
+    return 0;
+}
