ebecc8a7c06c92575606561fc47effc1cfd4f5f3
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-07-29 20:08:31 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240729/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240729/사각형 채우기 3/rectangle-fill-3.cpp
@@ -14,9 +14,9 @@ int main() {
     arr[1] = 2;
     
     for(int i=2; i<=n; i++) {
-        arr[i] = arr[i-1]*2 + arr[i-2]*3;
+        arr[i] = (arr[i-1]*2 + arr[i-2]*3)%MOD;
         for(int j=i-3; j>=0; j--) {
-            arr[i] += (arr[j]*2);
+            arr[i] = (arr[i] + arr[j]*2)%MOD;
         }
     }
 
