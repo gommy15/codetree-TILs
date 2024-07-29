f2e1db414cb95435d03d46d1b09910521179e9a9
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-07-29 20:13:59 +0900

    Title: 서로 다른 BST 개수 세기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240729/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 19
+
+int n;
+int dp[MAX_N+1];
+
+int NumOfUniqueBST(int num) {
+    int number_of_unique_bst = 0;
+
+    for(int i=0; i<num; i++) {
+        number_of_unique_bst += (dp[i] * dp[n-i-1]);
+    }
+
+    return number_of_unique_bst;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i=2; i<=n; i++) {
+        dp[i] = NumOfUniqueBST(i);
+    }
+
+    cout << dp[n];
+    return 0;
+}
