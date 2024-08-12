94c3f2c6954a3850a42fb29d2dd3ce43b5788445
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-12 13:03:48 +0900

    Title: 최대 점프 횟수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 1000
+
+int n;
+int dp[MAX_N], nums[MAX_N];
+
+void Init() {
+    for(int i=0; i<n; i++) {
+        dp[i] = INT_MIN;
+    }
+
+    dp[0] = 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    Init();
+
+    for(int i=1; i<n; i++) {
+        for(int j=0; j<i; j++) {
+            if(dp[j] == INT_MIN) continue;
+
+            if(j+nums[j] >= i) {
+                dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+    }
+
+    int ans = INT_MIN;
+    for(int i=0; i<n; i++) {
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+    return 0;
+}
