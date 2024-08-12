6be2c6497cd0a917cb10a6de8bd5e191dd10e00e
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-12 12:53:01 +0900

    Title: 최대 감소 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 1000
+
+int n;
+int dp[MAX_N+1], nums[MAX_N+1];
+
+void Init() {
+    for(int i=1; i<=n; i++) {
+        dp[i] = INT_MIN;
+    }
+
+    dp[0] = 0;
+    nums[0] = INT_MAX;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cin >> nums[i];
+    }
+
+    Init();
+
+    for(int i=1; i<=n; i++) {
+        for(int j=0; j<i; j++) {
+            if(nums[j] > nums[i])
+                dp[i] = max(dp[i], dp[j]+1);
+        }
+    }
+
+    int ans = 0;
+    for(int i=1; i<=n; i++) {
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+    
+    return 0;
+}
