e3d7e7185332513793e638aa512f058844a0d69b
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-14 11:51:18 +0900

    Title: 배낭 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240814/배낭 채우기/knapsack.cpp
+++ 240814/배낭 채우기/knapsack.cpp
@@ -5,25 +5,22 @@ using namespace std;
 #define MAX_N 100
 #define MAX_M 10000
 
+//처음 생각한 풀이
 int n, m;
-int dp[MAX_N+1][MAX_M+1];
-
+int dp[MAX_M+1];
 int weight[MAX_N+1], value[MAX_N+1];
 
 void Init() {
-    for(int i=0; i<=n; i++) {
-        for(int j=0; j<=m; j++) {
-            dp[i][j] = INT_MIN;
-        }
+    for(int i=0; i<=m; i++) {
+        dp[i] = INT_MIN;
     }
 
-    dp[0][0] = 0;
+    dp[0] = 0;
 }
 
-
 int main() {
-    // 여기에 코드를 작성해주세요.
     cin >> n>>m;
+
     for(int i=1; i<=n; i++) {
         cin >> weight[i] >> value[i];
     }
@@ -31,21 +28,68 @@ int main() {
     Init();
 
     for(int i=1; i<=n; i++) {
-        for(int j=0; j<=m; j++) {
-            if(j >= weight[i]) {
-                dp[i][j] = max(dp[i-1][j-weight[i]]+value[i], dp[i-1][j]);
-            } else {
-                dp[i][j] = dp[i-1][j];
-            }
+        for(int j=m; j>=weight[i]; j--) {
+            dp[j] = max(dp[j], dp[j-weight[i]]+value[i]);
         }
     }
 
     int ans = 0;
 
-    for(int j=0; j<=m; j++) {
-        ans = max(ans, dp[n][j]);
+    for(int i=0; i<=m; i++) {
+        ans = max(ans, dp[i]);
     }
 
     cout << ans;
-    return 0;
 }
+
+
+
+
+
+
+
+// 해설 풀이
+// int n, m;
+// int dp[MAX_N+1][MAX_M+1];
+
+// int weight[MAX_N+1], value[MAX_N+1];
+
+// void Init() {
+//     for(int i=0; i<=n; i++) {
+//         for(int j=0; j<=m; j++) {
+//             dp[i][j] = INT_MIN;
+//         }
+//     }
+
+//     dp[0][0] = 0;
+// }
+
+
+// int main() {
+//     // 여기에 코드를 작성해주세요.
+//     cin >> n>>m;
+//     for(int i=1; i<=n; i++) {
+//         cin >> weight[i] >> value[i];
+//     }
+
+//     Init();
+
+//     for(int i=1; i<=n; i++) {
+//         for(int j=0; j<=m; j++) {
+//             if(j >= weight[i]) {
+//                 dp[i][j] = max(dp[i-1][j-weight[i]]+value[i], dp[i-1][j]);
+//             } else {
+//                 dp[i][j] = dp[i-1][j];
+//             }
+//         }
+//     }
+
+//     int ans = 0;
+
+//     for(int j=0; j<=m; j++) {
+//         ans = max(ans, dp[n][j]);
+//     }
+
+//     cout << ans;
+//     return 0;
+// }
