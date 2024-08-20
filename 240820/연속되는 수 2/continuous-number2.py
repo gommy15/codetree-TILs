eb2058e7eddaad39c68434a104da88bca422b230
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-20 15:22:44 +0900

    Title: 연속되는 수 2, Time: 219ms, Memory: 72MB, Status: Passed - CodeTree

--- 240820/연속되는 수 2/continuous-number2.py
+++ 240820/연속되는 수 2/continuous-number2.py
@@ -5,16 +5,15 @@ nums = [
     for _ in range(n)
 ]
 
-max_cnt = 0
-cnt = 1
+max_cnt, cnt = 0, 0
 
 for i in range(n):
     if(i==0 or nums[i-1] != nums[i]):
-        max_cnt = max(max_cnt, cnt)
         cnt = 1
-        continue
-    
-    cnt += 1
+    else:
+        cnt += 1
+
+    max_cnt = max(max_cnt, cnt)
 
 
 print(max_cnt)
