787701b14979bb5cee8eb4c7b9b707f299311a32
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-20 16:30:49 +0900

    Title: 계단 오르기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/계단 오르기 2/climbing-stairs-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 1000
+
+int n;
+int coin[MAX_N+1], dp[MAX_N+1][4];
+
+void Init() {
+    for(int i=0; i<=n; i++) {
+        for(int j=0; j<=3; j++) {
+            dp[i][j] = INT_MIN;
+        }
+    }
+
+    dp[1][1] = coin[1];
+    dp[2][0] = coin[2];
+    dp[2][2] = coin[1]+coin[2];
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cin >> coin[i];
+    }
+
+    Init();
+
+    for(int i=3; i<=n; i++) {
+        for(int j=0; j<=3; j++) {
+            if(dp[i-2][j] != 0)
+                dp[i][j] = max(dp[i][j], dp[i-2][j] + coin[i]);
+            if(j && dp[i-1][j-1] != 0)
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + coin[i]);
+        }
+    }
+
+    int ans = 0;
+
+    for(int j=0; j<=3; j++) {
+        ans = max(ans, dp[n][j]);
+    }
+
+    cout << ans;
+
+    return 0;
+}
