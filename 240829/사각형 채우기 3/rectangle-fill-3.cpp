a0cbe73da0c26286d5e514b0d7b8330f763351fd
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-29 20:51:32 +0900

    Title: 사각형 채우기 3, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240829/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 1000
+#define MOD 1000000007
+
+int n;
+long long arr[MAX_N+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    arr[0] = 1;
+    arr[1] = 2;
+    
+    for(int i=2; i<=n; i++) {
+        arr[i] = (arr[i-1]*2 + arr[i-2]*3)%MOD;
+        for(int j=i-3; j>=0; j--) {
+            arr[i] = (arr[i] + arr[j]*2)%MOD;
+        }
+    }
+
+    cout << arr[n];
+
+    return 0;
+}
