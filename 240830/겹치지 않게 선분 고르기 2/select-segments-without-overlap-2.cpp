159b7a43a5c4f6dca033c8cf0d6d3761b8b96221
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-30 14:42:41 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240830/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <utility>
+#include <tuple>
+using namespace std;
+#define MAX_N 1000
+
+int n;
+pair<int, int> lines[MAX_N];
+int dp[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int x1, x2;
+        cin >> x1>>x2;
+        lines[i] = make_pair(x1, x2);
+    }
+
+    sort(lines, lines+n);
+    dp[0] = 1;
+
+    for(int i=1; i<n; i++) {
+        for(int j=0; j<i; j++) {
+            int a2, b1;
+            tie(ignore, a2) = lines[j];
+            tie(b1, ignore) = lines[i];
+
+            if(a2 < b1) {
+                dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+    }
+
+    int ans = 0;
+
+    for(int i=0; i<n; i++) {
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+    return 0;
+}
