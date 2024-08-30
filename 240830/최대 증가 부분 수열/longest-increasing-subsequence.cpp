965c41cf8a714209e17481f5eaf94061fe4ab3b9
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-30 14:12:51 +0900

    Title: 최대 증가 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240830/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 1000
+
+int n;
+int nums[MAX_N+1], dp[MAX_N+1];
+
+void Init() {
+    for(int i=1; i<=n; i++) {
+        dp[i] = INT_MIN;
+    }
+
+    dp[0] = 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cin >> nums[i];
+    }
+
+    Init();
+
+    for(int i=1; i<=n; i++) {
+        for(int j=0; j<i; j++) {
+            if(dp[j] == INT_MIN) continue;
+
+            if(nums[j] < nums[i])
+                dp[i] = max(dp[i], dp[j]+1);
+        }
+    }
+
+    cout << dp[n];
+    return 0;
+}
