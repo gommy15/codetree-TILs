86e0c80039e63bdfa2cb1076553adea66a21500f
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-08-30 14:58:05 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240830/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
+++ 240830/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
@@ -7,16 +7,6 @@ using namespace std;
 int n;
 int nums[MAX_N], dp[MAX_N][2];
 
-void Init() {
-    for(int i=1; i<n; i++) {
-        dp[i][0] = INT_MIN;
-        dp[i][1] = INT_MIN;
-    }
-
-    dp[0][0] = 1;
-    dp[0][1] = 1;
-}
-
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
@@ -24,17 +14,18 @@ int main() {
         cin >> nums[i];
     }
 
-    Init();
-
-    for(int i=1; i<n; i++) {
+    for(int i=0; i<n; i++) {
+        // 해당 위치에서 새로 시작할 때
+        dp[i][0] = 1;
+        dp[i][1] = 1;
         for(int j=0; j<i; j++) {
             // 증가할 때
-            if(dp[j][0] != INT_MIN && nums[j] < nums[i]) {
+            if(nums[j] < nums[i]) {
                 dp[i][0] = max(dp[i][0], dp[j][0]+1);
             }
 
             // 감소할 때
-            if(dp[j][1] != INT_MIN && nums[j] > nums[i]) {
+            if(nums[j] > nums[i]) {
                 dp[i][1] = max(dp[i][1], dp[j][1]+1);
             }
         }
