1ada1169d9405701004ab96c1d6831839aa63f96
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-09-04 17:43:26 +0900

    Title: 부분 수열의 합이 m, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240904/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 100
+#define MAX_M 10000
+
+int n, m;
+int arr[MAX_N], dp[MAX_M+1];
+
+void Init() {
+    for(int i=1; i<=m; i++) {
+        dp[i] = INT_MAX;
+    }
+
+    dp[0] = 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    Init();
+
+    for(int i=0; i<n; i++) {
+        for(int j=m; j>0; j--) {
+            if(j < arr[i]) continue;
+            if(dp[j-arr[i]] == INT_MAX) continue;
+
+            dp[j] = min(dp[j], dp[j-arr[i]]+1);
+        }
+    }
+
+    if (dp[m] == INT_MAX) cout << -1;
+    else cout << dp[m];
+
+    return 0;
+}
