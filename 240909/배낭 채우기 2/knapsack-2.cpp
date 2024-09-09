821fa91c112615d8b41275cbd60b5dfaa220176c
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-09-09 13:30:41 +0900

    Title: 배낭 채우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240909/배낭 채우기 2/knapsack-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX_N 100
+#define MAX_M 10000
+
+int n, m;
+int weight[MAX_N], value[MAX_N];
+int dp[MAX_M+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+
+    for(int i=0; i<n; i++) {
+        cin >> weight[i]>>value[i];
+    }
+
+    dp[0] = 0;
+
+    for(int i=1; i<=m; i++) {
+        for(int j=0; j<n; j++) {
+            if(i >= weight[j]) {
+                dp[i] = max(dp[i], dp[i-weight[j]]+value[j]);
+            }
+        }
+    }
+
+    cout << dp[m];
+    return 0;
+}
