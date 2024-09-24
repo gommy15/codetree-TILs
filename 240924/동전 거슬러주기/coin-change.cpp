dfa1c23c7abe1632e91c1238b8d5a07bb35ecbc4
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-09-24 16:54:13 +0900

    Title: 동전 거슬러주기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+#define MAX_N 100
+#define MAX_M 10000
+
+int n, m;
+int coin[MAX_N], dp[MAX_M+1];
+
+void Init() {
+    for(int i=1; i<=m; i++) {
+        dp[i] = INT_MAX;
+    }
+
+    dp[0] = 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n>>m;
+    for(int i=0; i<n; i++) {
+        cin >> coin[i];
+    }
+
+    Init();
+
+    for(int i=1; i<=m; i++) {
+        for(int j=0; j<n; j++) {
+            if(i >= coin[j] && dp[i-coin[j]] != INT_MAX) {
+                dp[i] = min(dp[i], dp[i-coin[j]]+1);
+            }
+        }
+    }
+
+    if(dp[m] == INT_MAX) cout << -1;
+    else cout << dp[m];
+    
+    return 0;
+}
