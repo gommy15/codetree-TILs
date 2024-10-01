8faabc21e0e22b988b57dd89a30632c791baf5dd
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-10-01 21:46:34 +0900

    Title: 트로미노, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/트로미노/tromino.cpp
+++ 241001/트로미노/tromino.cpp
@@ -9,7 +9,7 @@ int grid[MAX_NM][MAX_NM];
 int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
 
 bool InRange(int x, int y) {
-    return 0<=x && x<n && 0<=y && y<n;
+    return 0<=x && x<n && 0<=y && y<m;
 }
 
 int FindMax(int x, int y) {
