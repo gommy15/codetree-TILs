f6564766c4eba29a885934951d89d4449ac736ab
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-10-01 20:43:27 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 241001/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,52 +1,64 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-#define MAX_NM 100
+#define MAX_N 100
 
 int n, m;
-int grid[MAX_NM][MAX_NM];
+int grid[MAX_N][MAX_N];
+
+int seq[MAX_N];
+
+bool IsHappySequence() {
+    int cnt = 1, max_cnt = 1;
+
+    for(int i=1; i<n; i++) {
+        if(seq[i-1] == seq[i]) {
+            cnt++;
+        } else {
+            cnt = 1;
+        }
+
+        max_cnt = max(max_cnt, cnt);
+    }
+
+    return max_cnt >= m;
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n>>m;
+
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             cin >> grid[i][j];
         }
     }
 
-    int ans = 0;
+    int num_happy = 0;
 
+    // 가로 확인
     for(int i=0; i<n; i++) {
-        int cnt = 1;
-        int max_cnt = 0;
-        for(int j=1; j<n; j++) {
-            if(grid[i][j-1] == grid[i][j]) {
-                cnt++;
-                max_cnt = max(max_cnt, cnt);
-            } else {
-                cnt = 1;
-            }
+        for(int j=0; j<n; j++) {
+            seq[j] = grid[i][j];
         }
 
-        if (max_cnt >= m) ans++;
+        if(IsHappySequence()) {
+            num_happy++;
+        }
     }
 
-    for(int i=0; i<n; i++) {
-        int cnt = 1;
-        int max_cnt = 0;
-        for(int j=1; j<n; j++) {
-            if(grid[j-1][i] == grid[j][i]) {
-                cnt++;
-                max_cnt = max(max_cnt, cnt);
-            } else {
-                cnt = 1;
-            }
+    //세로 확인
+    for(int j=0; j<n; j++) {
+        for(int i=0; i<n; i++) {
+            seq[i] = grid[i][j];
         }
 
-        if (max_cnt >= m) ans++;
+        if(IsHappySequence()) {
+            num_happy++;
+        }
     }
 
-    cout << ans;
+
+    cout << num_happy;
     return 0;
 }
