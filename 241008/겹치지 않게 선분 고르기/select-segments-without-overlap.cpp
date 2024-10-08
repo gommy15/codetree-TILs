ff44195964a33eb0e478a0d46949816a990fab63
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-10-08 19:16:39 +0900

    Title: 겹치지 않게 선분 고르기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241008/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 241008/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -3,49 +3,60 @@
 #include <vector>
 #include <tuple>
 using namespace std;
+#define MAX_N 15
 
-int n, ans;
-vector<pair<int, int>> lines, selec_l;
+int n;
+pair<int, int> segments[MAX_N];
 
-void FindMax(int cnt) {
+int ans;
+vector<pair<int, int>> selected;
+
+bool Overlapped(pair<int, int> seg1, pair<int, int> seg2) {
+    int ax1, ax2;
+    tie(ax1, ax2) = seg1;
+
+    int bx1, bx2;
+    tie(bx1, bx2) = seg2;
+
+    return (ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) || (bx1 <= ax1 && ax1 <= bx2) || (bx1 <= ax2 && ax2 <= bx2);
+}
+
+bool Possible() {
+    for(int i=0; i<selected.size(); i++) {
+        for(int j=i+1; j<selected.size(); j++) {
+            if(Overlapped(selected[i], selected[j]))
+                return false;
+        }
+    }
+    return true;
+}
+
+void FindMaxSegments(int cnt) {
     if(cnt == n) {
-        ans = max(ans, (int)selec_l.size());
+        if(Possible()) {
+            ans = max(ans, (int)selected.size());
+        }
         return;
     }
 
-    if(selec_l.size() == 0) {
-        selec_l.push_back(lines[cnt]);
-        FindMax(cnt+1);
-        selec_l.pop_back();
-    } else {
-        int x1, x2;
-        tie(ignore, x2) = selec_l.back();
-        tie(x1, ignore) = lines[cnt];
+    selected.push_back(segments[cnt]);
+    FindMaxSegments(cnt+1);
+    selected.pop_back();
 
-        if(x2 < x1) {
-            selec_l.push_back(lines[cnt]);
-            FindMax(cnt+1);
-            selec_l.pop_back();
-        } else {
-            FindMax(cnt+1);
-        }
-    }
+    FindMaxSegments(cnt+1);
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     cin >> n;
+
     for(int i=0; i<n; i++) {
         int x1, x2;
         cin >> x1>>x2;
-        lines.push_back(make_pair(x1, x2));
+        segments[i] = make_pair(x1, x2);
     }
 
-    sort(lines.begin(), lines.end());
-
-    FindMax(0);
-
-    cout << ans;
+    FindMaxSegments(0);
     
+    cout << ans;
     return 0;
 }
