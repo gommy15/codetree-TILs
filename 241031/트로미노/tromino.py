8ad05c1d1b2dd04a9bd110a7b3203b16b7b95deb
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-10-31 14:16:19 +0900

    Title: 트로미노, Time: 242ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 241031/트로미노/tromino.py
@@ -0,0 +1,51 @@
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def InRange(x, y):
+    return 0<=x and x<n and 0<=y and y<m
+
+def Sum1(x, y):
+    summ = 0
+
+    for d in range(4):
+        nx1 = x+dx[d]
+        ny1 = y+dy[d]
+
+        nx2 = x+dx[(d+1)%4]
+        ny2 = y+dy[(d+1)%4]
+
+        if InRange(nx1, ny1) and InRange(nx2, ny2):
+            summ = max(summ, grid[x][y]+grid[nx1][ny1]+grid[nx2][ny2])
+    
+    return summ
+
+def Sum2(x, y):
+    summ = 0
+
+    for d in range(2):
+        nx1 = x+dx[d]
+        ny1 = y+dy[d]
+
+        nx2 = x+dx[d+2]
+        ny2 = y+dy[d+2]
+
+        if InRange(nx1, ny1) and InRange(nx2, ny2):
+            summ = max(summ, grid[x][y]+grid[nx1][ny1]+grid[nx2][ny2])
+    
+    return summ
+
+ans = 0
+
+for i in range(n):
+    for j in range(m):
+
+        ans = max(ans, Sum1(i, j))
+        ans = max(ans, Sum2(i, j))
+
+print(ans)
