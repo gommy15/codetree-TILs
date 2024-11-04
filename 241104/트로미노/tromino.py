8d55d62817e216dcc5f5c2e8082b6fed0a9510f0
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-11-04 12:26:28 +0900

    Title: 트로미노, Time: 47ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241104/트로미노/tromino.py
@@ -0,0 +1,42 @@
+n, m = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+ans = 0
+
+def inRange(x, y):
+    return 0<=x<n and 0<=y<n
+
+def calc1(x, y):
+
+    result = 0
+    for d in range(4):
+        nx1, ny1 = x+dx[d], y+dy[d]
+        nx2, ny2 = x+dx[(d+1)%4], y+dy[(d+1)%4]
+
+        if inRange(nx1, ny1) and inRange(nx2, ny2):
+            result = max(result, grid[x][y]+grid[nx1][ny1]+grid[nx2][ny2])
+    
+    return result
+
+def calc2(x, y):
+    result = 0
+    for d in range(2):
+        nx1, ny1 = x+dx[d], y+dy[d]
+        nx2, ny2 = x+dx[(d+2)%4], y+dy[(d+2)%4]
+
+        if inRange(nx1, ny1) and inRange(nx2, ny2):
+            result = max(result, grid[x][y]+grid[nx1][ny1]+grid[nx2][ny2])
+    
+    return result
+
+for i in range(n):
+    for j in range(m):
+        ans = max(ans, calc1(i, j))
+        ans = max(ans, calc2(i, j))
+
+print(ans)
