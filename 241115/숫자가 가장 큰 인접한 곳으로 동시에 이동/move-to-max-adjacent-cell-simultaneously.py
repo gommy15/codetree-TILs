b6939ceabfe1319ee1cf85b5888719596f529122
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-11-15 22:01:25 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241115/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 241115/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -5,17 +5,17 @@ grid = [
     for _ in range(n)
 ]
 
-ball = [[0]*n for _ in range(n)]
+#ball = [[0]*n for _ in range(n)]
 
 ball_spot = []
 
 for _ in range(m):
     r, c = tuple(map(int, input().split()))
     ball_spot.append((r-1, c-1))
-    ball[r-1][c-1] = 1
 
 def move():
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+    new_ball = [[0]*n for _ in range(n)]
 
     for x, y in ball_spot:
         max_num = grid[x][y]
@@ -28,25 +28,21 @@ def move():
                 max_pos = (nx, ny)
                 max_num = grid[nx][ny]
         
-        ball[x][y] -= 1
-        nx, ny = max_pos
-        ball[nx][ny] += 1
+        if max_num != grid[x][y]:
+            nx, ny = max_pos
+            new_ball[nx][ny] += 1
 
     ball_spot.clear()
-    
+
     for i in range(n):
         for j in range(n):
-            if ball[i][j] == 0:
-                continue
-            elif ball[i][j] == 1:
+            if new_ball[i][j] == 1:
                 ball_spot.append((i, j))
-            else:
-                ball[i][j] = 0
 
 
 for _ in range(t):
     move()
 
-ans = sum(sum(r) for r in ball)
+ans = len(ball_spot)
 
 print(ans)
