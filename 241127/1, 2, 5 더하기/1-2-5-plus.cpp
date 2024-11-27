c323973f21371fcee124405c0a840145aa0e03be
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2024-11-27 20:48:40 +0900

    Title: 1, 2, 5 더하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241127/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 1000
+#define MAX_M 3
+#define MOD 10007
+
+int n;
+int nums[MAX_M] = {1, 2, 5};
+int dp[MAX_N+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    dp[0] = 1;
+
+    for(int i=1; i<=n; i++) {
+        for(int j=0; j<MAX_M; j++) {
+            if(i-nums[j] >= 0) {
+                dp[i] += dp[i-nums[j]];
+            }
+        }
+    }
+
+    cout << dp[n];
+    return 0;
+}
