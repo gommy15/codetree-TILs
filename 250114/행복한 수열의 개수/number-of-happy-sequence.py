1ac67746bc6e9b99ca4d2ff5937b9513075e9d23
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-01-14 20:34:50 +0900

    Title: 행복한 수열의 개수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250114/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,43 @@
+n, m = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+seq = [0] * n
+ans = 0
+
+def isHappy():
+    cnt = 1
+    max_cnt = 0
+
+    for i in range(1, n):
+        if seq[i-1] == seq[i]:
+            cnt += 1
+        else:
+            cnt = 1
+        
+        max_cnt = max(cnt, max_cnt)
+    
+    if max_cnt >= m:
+        return True
+    else:
+        return False
+
+for i in range(n):
+    for j in range(n):
+        seq[j] = grid[i][j]
+    
+    if isHappy():
+        ans += 1
+
+
+for i in range(n):
+    for j in range(n):
+        seq[j] = grid[j][i]
+    
+    if isHappy():
+        ans += 1
+
+print(ans)
