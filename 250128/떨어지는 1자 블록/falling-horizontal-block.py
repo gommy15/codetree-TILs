0783e5efa02e5e6f57d824a3d0007d7b666b8438
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-01-28 20:54:20 +0900

    Title: 떨어지는 1자 블록, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250128/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,31 @@
+n, m, k = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+def all_blank(r, s, e):
+    return all([
+        not grid[r][c]
+        for c in range(s, e+1)
+    ])
+
+def get_row():
+    for r in range(n-1):
+        if not all_blank(r+1, k, k+m-1):
+            return r
+    
+    return n-1
+
+k -= 1
+
+target = get_row()
+
+for c in range(k, k+m):
+    grid[target][c] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
+    print()
