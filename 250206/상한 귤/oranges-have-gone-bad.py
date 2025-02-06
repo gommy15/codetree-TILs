26e3233b86846da31c27910451147de03533bea9
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-02-06 16:25:48 +0900

    Title: 상한 귤, Time: 128ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 250206/상한 귤/oranges-have-gone-bad.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+n, k = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+q = deque()
+visited = [[-1 for _ in range(n)] for _ in range(n)]
+
+cur_dam = [
+    (i, j)
+    for i in range(n)
+    for j in range(n)
+    if grid[i][j] == 2
+]
+
+def can_go(x, y):
+    return 0<=x<n and 0<=y<n and grid[x][y] == 1 and visited[x][y] == -1
+
+def bfs():
+    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+
+            if can_go(nx, ny):
+                q.append((nx, ny))
+                visited[nx][ny] = visited[x][y]+1
+
+
+for x, y in cur_dam:
+    visited[x][y] = 0
+    q.append((x, y))
+
+bfs()
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1 and visited[i][j] == -1:
+            print(-2, end = " ")
+        else:
+            print(visited[i][j], end = " ")
+    
+    print()
