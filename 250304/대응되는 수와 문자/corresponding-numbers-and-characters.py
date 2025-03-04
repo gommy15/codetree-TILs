fc28801ac93a012c2083688804f4d7383162c8a6
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-03-04 15:42:12 +0900

    Title: 대응되는 수와 문자, Time: 381ms, Memory: 48MB, Status: Passed - CodeTree

---
+++ 250304/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -0,0 +1,18 @@
+n, m = tuple(map(int, input().split()))
+
+stoi = dict()
+itos = dict()
+
+for i in range(1, n+1):
+    st = input()
+    stoi[st] = i
+    itos[str(i)] = st
+
+for i in range(m):
+    inp = input()
+
+    if '0' <= inp[0] <= '9':
+        print(itos[inp])
+    else:
+        print(stoi[inp])
+
