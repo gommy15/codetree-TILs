668d35db9158f22ebeddec1490f7fbb74db92fb4
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-03-20 21:34:14 +0900

    Title: 겹치지 않게 선분 고르기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250320/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ans;
+vector<pair<int, int>> line;
+vector<pair<int, int>> cho;
+
+bool Check() {
+    sort(cho.begin(), cho.end());
+
+    for(int i=1; i<(int)cho.size(); i++) {
+        int x, y;
+        tie(x, y) = cho[i-1];
+
+        int x2, y2;
+        tie(x2, y2) = cho[i];
+
+        if(!(y < x2)) return false;
+    }
+
+    return true;
+}
+
+void FindMaxLine(int idx) {
+    if(idx == (int)line.size()) {
+        if(Check()) {
+            ans = max(ans, (int)cho.size());
+        }
+        return;
+    }
+
+    cho.push_back(line[idx]);
+    FindMaxLine(idx + 1);
+    cho.pop_back();
+
+    FindMaxLine(idx+1);
+}
+
+int main() {
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        int x, y;
+        cin >> x>>y;
+        line.push_back({x, y});
+    }
+
+    FindMaxLine(0);
+
+    cout << ans;
+    return 0;
+}
