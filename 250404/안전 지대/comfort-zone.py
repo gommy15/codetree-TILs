ce7f21b62bde06259ec16b6270bb5fe09467a398
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-04-04 16:13:50 +0900

    Title: 안전 지대, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250404/안전 지대/comfort-zone.py
+++ 250404/안전 지대/comfort-zone.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.setrecursionlimit(2500)
+
 n, m = tuple(map(int, input().split()))
 
 grid = [
@@ -26,7 +30,7 @@ def dfs(x, y, k):
             visited[nx][ny] = True
             dfs(nx, ny, k)
 
-ans_k = 1
+ans_k = 0
 ans = 0
 
 
