bc47c8666c629cdb09a0010d122879676a9befd7
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-04-22 22:33:58 +0900

    Title: 두 수의 합, Time: 82ms, Memory: 26MB, Status: Passed - Codetree

--- 250422/두 수의 합/sum-of-two-num.py
+++ 250422/두 수의 합/sum-of-two-num.py
@@ -1,18 +1,22 @@
-n, m = tuple(map(int, input().split()))
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-stoi = dict()
-itos = dict()
+count = dict()
 
-for i in range(1, n+1):
-    st = input()
-    stoi[st] = i
-    itos[chr(i)] = st
+ans = 0
+# 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+for elem in arr:
+    diff = k - elem
+    # 가능한 모든 쌍의 수를 세어줍니다.
+    if diff in count:
+        ans += count[diff]
 
-for i in range(m):
-    inp = input()
-
-    if '0' <= inp[0] <= '9':
-        print(itos[inp])
+    # 현재 숫자의 개수를 하나 증가시켜줍니다.
+    if elem in count:
+        count[elem] += 1
     else:
-        print(stoi[inp])
+        count[elem] = 1
+
+print(ans)
 
