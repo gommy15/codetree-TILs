f380896784910aeabcd67f91437a2a2ea901069f
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-05-13 12:19:19 +0900

    Title: 최대 점프 횟수, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250513/최대 점프 횟수/maximum-number-of-jumps.py
+++ 250513/최대 점프 횟수/maximum-number-of-jumps.py
@@ -2,13 +2,15 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-dp = [0] * n
+dp = [-1] * n
+
+dp[0] = -
 
 for i in range(n):
     for j in range(0, i):
-        if j != 0 and dp[j] == 0:
+        if dp[j] == -1:
             continue
-            
+
         if j+arr[j] >= i:
             dp[i] = max(dp[i], dp[j]+1)
     
