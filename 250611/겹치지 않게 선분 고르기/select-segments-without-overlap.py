ad3d810d311f888ab3acab8cf34006ce444d8ea7
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-06-11 22:46:33 +0900

    Title: 겹치지 않게 선분 고르기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250611/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,40 @@
+n = int(input())
+
+lines = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+lines.sort()
+selc_line = []
+
+ans = 0
+
+def check():
+    for i in range(1, len(selc_line)):
+        _, y = selc_line[i-1]
+        x, _ = selc_line[i]
+
+        if y >= x:
+            return False
+    
+    return True
+
+def find_max_line(cnt):
+    global ans
+
+    if cnt == n:
+        if check():
+            ans = max(ans, len(selc_line))
+        return
+    
+    selc_line.append(lines[cnt])
+    find_max_line(cnt+1)
+    selc_line.pop()
+
+    find_max_line(cnt+1)
+
+
+find_max_line(0)
+
+print(ans)
