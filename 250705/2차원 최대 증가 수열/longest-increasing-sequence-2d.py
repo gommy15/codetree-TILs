f2f8cbe10f053ef04db29265b548e3c4369fb968
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-07-05 15:54:45 +0900

    Title: 2차원 최대 증가 수열, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250705/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 250705/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,3 +1,7 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
 n, m = tuple(map(int, input().split()))
 
 grid = [
@@ -5,14 +9,17 @@ grid = [
     for _ in range(n)
 ]
 
-dp = [[0]*n for _ in range(n)]
+dp = [[INT_MIN]*m for _ in range(n)]
 
 dp[0][0] = 1
 
 for i in range(1, n):
-    for j in range(1, n):
+    for j in range(1, m):
         for k in range(i):
             for l in range(j):
+                if dp[k][l] == INT_MIN:
+                    continue
+
                 if grid[k][l] < grid[i][j]:
                     dp[i][j] = max(dp[i][j], dp[k][l]+1)
 
