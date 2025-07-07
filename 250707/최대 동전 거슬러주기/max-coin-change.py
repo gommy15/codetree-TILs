f034cf0a60757449b7011addebb11d6912c60428
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-07-07 17:14:02 +0900

    Title: 최대 동전 거슬러주기, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250707/최대 동전 거슬러주기/max-coin-change.py
+++ 250707/최대 동전 거슬러주기/max-coin-change.py
@@ -8,7 +8,7 @@ dp[0] = 0
 
 for i in range(1, m+1):
     for coin in coins:
-        if i >= coin:
+        if i >= coin and dp[i-coin] != -1:
             dp[i] = max(dp[i], dp[i-coin]+1)
 
 print(dp[m])
