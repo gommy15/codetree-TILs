c0c0d64a5b06fc57e076162889e03948cf09888d
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-09-26 18:13:04 +0900

    Title: K번 최댓값으로 이동하기, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250926/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,81 @@
+import collections
+
+NOT_EXISTS = (-1, -1)
+
+n, k = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+r, c = tuple(map(int, input().split()))
+cur_cell = (r-1, c-1)
+
+q = collections.deque()
+
+visited = [[0]*n for _ in range(n)]
+
+def can_go(x, y, t):
+    return 0<=x<n and 0<=y<n and not visited[x][y] and grid[x][y] < t
+
+def bfs():
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    x, y = cur_cell
+    visited[x][y] = True
+    q.append(cur_cell)
+
+    targ = grid[x][y]
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+
+            if can_go(nx, ny, targ):
+                q.append((nx, ny))
+                visited[nx][ny] = True
+
+def need_update(bp, np):
+    if bp == NOT_EXISTS:
+        return True
+    
+    bx, by = bp
+    nx, ny = np
+
+    return (grid[nx][ny], -nx, -ny) > (grid[bx][by], -bx, -by)
+
+def move():
+    global cur_cell, visited
+
+    visited = [[0]*n for _ in range(n)]
+
+    bfs()
+
+    best_pos = NOT_EXISTS
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j] or (i, j) == cur_cell:
+                continue
+            
+            new_pos = (i, j)
+            if need_update(best_pos, new_pos):
+                best_pos = new_pos
+    
+    if best_pos == NOT_EXISTS:
+        return False
+    else:
+        cur_cell = best_pos
+        return True
+
+
+
+for _ in range(k):
+    is_moved = move()
+
+    if not is_moved:
+        break
+
+x, y = cur_cell
+print(x+1, y+1)
