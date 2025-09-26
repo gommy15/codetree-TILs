5ff5d15ac141eb2893f722eec4417a00a80b6bb9
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-09-26 14:14:59 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250926/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 250926/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -7,16 +7,17 @@ arr = list(map(int, input().split()))
 
 ans = INT_MAX
 
-def find_min_diff(idx, diff):
+def find_min_diff(idx, cnt, diff):
     global ans
 
     if idx == 2*n:
-        ans = min(ans, abs(diff))
+        if cnt == n:
+            ans = min(ans, abs(diff))
         return
     
-    find_min_diff(idx+1, diff+arr[idx])
-    find_min_diff(idx+1, diff-arr[idx])
+    find_min_diff(idx+1, cnt+1, diff+arr[idx])
+    find_min_diff(idx+1, cnt, diff-arr[idx])
 
-find_min_diff(0, 0)
+find_min_diff(0, 0, 0)
 
 print(ans)
