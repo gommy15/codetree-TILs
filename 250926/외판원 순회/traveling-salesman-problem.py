a4b83dd9ffa004d5a57cf794d3bdc106219173d2
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-09-26 15:16:55 +0900

    Title: 외판원 순회, Time: 234ms, Memory: 23MB, Status: Passed - Codetree

--- 250926/외판원 순회/traveling-salesman-problem.py
+++ 250926/외판원 순회/traveling-salesman-problem.py
@@ -3,49 +3,40 @@ import sys
 INT_MAX = sys.maxsize
 
 n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
-
-visited = [False] * (n+1)
-course = [1]
+visited = [False] * n
+course = [0] 
 
 ans = INT_MAX
 
 def calc():
     dist = 0
-
-    for i in range(n):
-        if grid[course[i]-1][course[i+1]-1] == 0:
+    for i in range(n - 1):
+        if grid[course[i]][course[i+1]] == 0:
             return INT_MAX
-            
-        dist += grid[course[i]-1][course[i+1]-1]
-    
-    dist += grid[course[n]-1][0]
+        dist += grid[course[i]][course[i+1]]
 
+    if grid[course[-1]][course[0]] == 0:
+        return INT_MAX
+    dist += grid[course[-1]][course[0]]
     return dist
 
 def find_min_dist(cnt):
     global ans
-
-    if cnt == n:
+    if cnt == n - 1:
         ans = min(ans, calc())
         return
-    
-    for i in range(1, n+1):
+    for i in range(1, n):
         if visited[i]:
             continue
-        
+
         course.append(i)
         visited[i] = True
-
         find_min_dist(cnt+1)
-
         course.pop()
         visited[i] = False
 
+visited[0] = True  
 find_min_dist(0)
-
 print(ans)
