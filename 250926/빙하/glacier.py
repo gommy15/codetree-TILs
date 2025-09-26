d63fe3a3a01da3cbbe15e6a1106e70f6a68f16c1
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-09-26 20:39:25 +0900

    Title: 빙하, Time: 147ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 250926/빙하/glacier.py
@@ -0,0 +1,70 @@
+from collections import deque
+
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+q = deque()
+melting = deque()
+visited = [[False]*m for _ in range(n)]
+cnt = 0
+
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+t = 0
+melt_cnt = 0
+
+q.append((0, 0))
+visited[0][0] = True
+
+def can_go(x, y):
+    return 0<=x<n and 0<=y<m and grid[x][y] == 0 and not visited[x][y]
+
+def is_glac(x, y):
+    return 0<=x<n and 0<=y<m and grid[x][y] == 1 and not visited[x][y]
+
+def bfs():
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+
+            if can_go(nx, ny):
+                q.append((nx, ny))
+                visited[nx][ny] = True
+            elif is_glac(nx, ny):
+                melting.append((nx, ny))
+                visited[nx][ny] = True
+
+def melt():
+    while melting:
+        x, y = melting.popleft()
+        grid[x][y] = 0
+
+def simulate():
+    global t, melt_cnt, q
+
+    bfs()
+
+    if not melting:
+        return False
+    
+    t += 1
+    melt_cnt = len(melting)
+
+    q = melting.copy()
+
+    melt()
+
+    return True
+
+while True:
+    is_exist = simulate()
+
+    if not is_exist:
+        break
+
+print(t, melt_cnt)
