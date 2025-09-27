e114a767696845f6b9833ea60a0961ccf00c4321
Heerae Lee <64458827+gommy15@users.noreply.github.com>
2025-09-27 09:05:55 +0900

    Title: 배낭 채우기, Time: 101ms, Memory: 25MB, Status: Passed - Codetree

---
+++ 250927/배낭 채우기/knapsack.py
@@ -0,0 +1,22 @@
+n, m = tuple(map(int, input().split()))
+
+info = [[0, 0]] + [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+dp = [[0]*(m+1) for _ in range(n+1)]
+
+dp[0][0] = 0
+
+for i in range(1, n+1):
+    for j in range(m+1):
+        if j >= info[i][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-info[i][0]] + info[i][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[n][m])
+
+
+
